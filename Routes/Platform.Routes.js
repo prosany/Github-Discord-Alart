f022794016229d358ea8542ddf49798a31bbc68d
Sunny <mahabubsany2@gmail.com>
2022-10-29 22:43:25 +0600

    added

--- Routes/Platform.Routes.js
+++ Routes/Platform.Routes.js
@@ -1,16 +1,31 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/alert", (req, res) => {
-  try {
-    console.log(req.body);
-    console.log(req.headers);
-  } catch (error) {
-    res.status(500).send({
-      status: 0,
-      message: "Internal Server Error.",
-    });
-  }
-});
+router
+  .route("/alert")
+  .post((req, res) => {
+    try {
+      console.log(req.body);
+      console.log(req.headers);
+    } catch (error) {
+      res.status(500).send({
+        status: 0,
+        message: "Internal Server Error.",
+      });
+    }
+  })
+  .get((req, res) => {
+    try {
+      res.status(200).send({
+        status: 1,
+        message: "Alert working!",
+      });
+    } catch (error) {
+      res.status(500).send({
+        status: 0,
+        message: "Internal Server Error.",
+      });
+    }
+  });
 
 module.exports = router;
